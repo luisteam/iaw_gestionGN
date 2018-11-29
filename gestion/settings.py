d8fad8f4f192a55f54728abea55782acba7f0e42
luisteam <luis_rayita@hotmail.es>
2018-11-29 20:18:03 +0100

    Cambios para pythonanywhere

--- gestion/settings.py
+++ gestion/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'proyectogestiona_db',
-        'USER': 'luis',
-        'PASSWORD':'root',
+        'USER': 'luisteam',
+        'PASSWORD':'root1234',
+        'HOST': 'luisteam.mysql.pythonanywhere-services.com',
+        'PORT': '',
     }
 }
 
